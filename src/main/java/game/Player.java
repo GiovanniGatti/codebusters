fbca542cf4d6d50e7beb577b5065f52ff262592c
U-Giovanni-Think\Giovanni <giovanni.gatti.pinheiro@gmail.com>
2016-06-25 18:17:47 +0200

    Start up

--- src/main/java/game/Player.java
+++ src/main/java/game/Player.java
@@ -2,11 +2,36 @@ package game;
 
 import java.util.Collections;
 import java.util.Map;
+import java.util.Scanner;
 
 final class Player {
 
     public static void main(String args[]) {
-        // TODO: implement me!
+        Scanner in = new Scanner(System.in);
+        int bustersPerPlayer = in.nextInt(); // the amount of busters you control
+        int ghostCount = in.nextInt(); // the amount of ghosts on the map
+        int myTeamId = in.nextInt(); // if this is 0, your base is on the top left of the map, if it is one, on the bottom right
+
+        // game loop
+        while (true) {
+            int entities = in.nextInt(); // the number of busters and ghosts visible to you
+            for (int i = 0; i < entities; i++) {
+                int entityId = in.nextInt(); // buster id or ghost id
+                int x = in.nextInt();
+                int y = in.nextInt(); // position of this buster / ghost
+                int entityType = in.nextInt(); // the team id if it is a buster, -1 if it is a ghost.
+                int state = in.nextInt(); // For busters: 0=idle, 1=carrying a ghost.
+                int value = in.nextInt(); // For busters: Ghost id being carried. For ghosts: number of busters attempting to trap this ghost.
+            }
+
+            for (int i = 0; i < bustersPerPlayer; i++) {
+
+                // Write an action using System.out.println()
+                // To debug: System.err.println("Debug messages...");
+
+                System.out.println("MOVE 8000 4500"); // MOVE x y | BUST id | RELEASE
+            }
+        }
     }
 
     static abstract class AI {
@@ -24,7 +49,7 @@ final class Player {
 
         /**
          * Implements the IA algorithm
-         * 
+         *
          * @param current the current state
          * @return the best action found
          */
@@ -58,7 +83,7 @@ final class Player {
 
         /**
          * Performs an action (which will mutate the game state)
-         * 
+         *
          * @param action to perform
          */
         void perform(Action action) {
